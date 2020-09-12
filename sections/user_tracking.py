ea3dd869fb6ed68231eed2a64501f5cd43063e4d
Margret `Pax` Williams <pax.margret@gmail.com>
2020-09-12 13:02:45 -0700

    Implement skeleton of User-Tracking screen

--- sections/user_tracking.py
+++ sections/user_tracking.py
@@ -1,14 +1,34 @@
+from datetime import datetime as dt
 import dash_core_components as dcc
 import dash_html_components as html
 
+
 user_tracking_section = html.Div(
     children=[
-        html.H1(children="User Tracking"),
-        html.Div(
-            children="""
-        [ TODO ENTER COPY HERE ]
-    """
+        html.H1(children="Plastic Footprint"),
+        html.P(children="By default you are seeing..."),
+        dcc.DatePickerSingle(
+            id="date-picker",
+            date=dt.today(),
         ),
+        html.P(children="Plastic Waste"),
+        # TODO UI looks like this should be a button?
+        dcc.RadioItems(
+            options=[
+                {"label": "Film", "value": "Film"},
+                {"label": "Textiles", "value": "Textiles"},
+                {
+                    "label": "Rigid Beverage Containers",
+                    "value": "Rigid Beverage Containers",
+                },
+                {
+                    "label": "Rigid Non-Beverage Containers",
+                    "value": "Rigid Non-Beverage Containers",
+                },
+                {"label": "Other", "value": "Other"},
+            ]
+        ),
+        html.Button("Track", id="track"),
     ],
     style={"margin-right": "50px"},
 )
