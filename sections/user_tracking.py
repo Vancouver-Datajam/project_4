76c2cd48d01373d9df680a341c79bc9f6f077fe7
Margret `Pax` Williams <pax.margret@gmail.com>
2020-09-12 16:17:42 -0700

    Add spacing

--- sections/user_tracking.py
+++ sections/user_tracking.py
@@ -50,7 +50,7 @@ user_tracking_section = html.Div(
             min=1,
             style={"margin-bottom": "40px"},
         ),
-        html.Button("Track", id="track-button"),
+        html.Button("Track", id="track-button", style={"margin-bottom": "10px"}),
         html.P(id="on-track-button-submit-output", children=""),
     ],
     style={"margin-right": "50px", "width": "300px"},
