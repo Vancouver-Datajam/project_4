4a524ee22c1edcf09b6912f4cc18c7390f29b9c0
Margret `Pax` Williams <pax.margret@gmail.com>
2020-09-12 14:33:33 -0700

    Fix spacing in User Tracking form

--- sections/user_tracking.py
+++ sections/user_tracking.py
@@ -6,14 +6,22 @@ import dash_html_components as html
 user_tracking_section = html.Div(
     children=[
         html.H1(children="Plastic Footprint"),
-        html.P(children="By default you are seeing..."),
+        html.P(
+            children="""
+            By default you are seeing a graph of Vancouver's Plastic Waste.
+             Track your plastic waste to see how you compare with the city.
+        """,
+            style={"margin-bottom": "40px"},
+        ),
+        html.Label(children="Date", style={"font-weight": "bold"}),
         dcc.DatePickerSingle(
             id="date-picker",
             min_date_allowed=dt(2000, 8, 5),
             max_date_allowed=dt.today(),
             date=dt.today(),
+            style={"margin-bottom": "40px"},
         ),
-        html.P(children="Plastic Waste"),
+        html.Label(children="Plastic Waste", style={"font-weight": "bold"}),
         # TODO UI looks like this should be a button?
         dcc.RadioItems(
             id="plastic-type",
@@ -31,9 +39,10 @@ user_tracking_section = html.Div(
                 },
                 {"label": "Other", "value": "other"},
             ],
+            style={"margin-bottom": "40px"},
         ),
         html.Button("Track", id="track-button"),
         html.P(id="on-track-button-submit-output", children=""),
     ],
-    style={"margin-right": "50px"},
+    style={"margin-right": "50px", "width": "300px"},
 )
