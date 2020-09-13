ddf8cbfd061d3ed9ecfdf6276450256ceb41d965
Pax <13646646+paxcodes@users.noreply.github.com>
2020-09-12 17:30:14 -0700

    Add Count textfield (#7)
    * Add count textfield
    
    * Add default value
    
    * Add spacing
    
    * New data using count field
    
    * typo in comment

--- app.py
+++ app.py
@@ -45,9 +45,10 @@ app.layout = html.Div(
     [
         dash.dependencies.State("plastic-type", "value"),
         dash.dependencies.State("date-picker", "date"),
+        dash.dependencies.State("count-field", "value"),
     ],
 )
-def on_track_submit(n_clicks, plastic_type_value, date_value):
+def on_track_submit(n_clicks, plastic_type_value, date_value, count_value):
     print(f"Entries! [Num Clicks: {n_clicks}] {plastic_type_value} {date_value}")
     if n_clicks is None:
         return ""
@@ -56,11 +57,15 @@ def on_track_submit(n_clicks, plastic_type_value, date_value):
     date_value_obj = dt.strptime(re.split("T| ", date_value)[0], "%Y-%m-%d")
     # Format it according to our needs: MM/DD/YYYY
     date_string = date_value_obj.strftime("%m/%d/%Y")
+
+    weight_kg_plastic_type = utils.get_weight_kg_of_plastic(plastic_type_value)
+    weight_kg_total = weight_kg_plastic_type * count_value
+    # Rounding off to 3 decimal places
+    weight_kg_total_str = "{0:.3f}".format(weight_kg_total)
     data = {
         "plastic_family": plastic_type_value,
-        "weight_kg": str(utils.get_weight_kg_of_plastic(plastic_type_value)),
-        "count": "1",
-        # TODO
+        "weight_kg": weight_kg_total_str,
+        "count": str(count_value),
         "date": date_string,
     }
 
@@ -68,7 +73,10 @@ def on_track_submit(n_clicks, plastic_type_value, date_value):
     with open(USER_DATA_FILE, "a") as f:
         f.write(f"{new_entry}\n")
 
-    return "Tracked!"
+    return f"""
+        Recorded {count_value} {plastic_type_value} 
+        (estimated {weight_kg_total_str} kg) for {date_string}
+    """
 
 
 if __name__ == "__main__":
